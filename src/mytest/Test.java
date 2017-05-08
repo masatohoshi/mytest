9d7a7363388c7d6dd01dcda0be04b2bee7bbc3ea
HOSHI <mhoshi@pentaho.com>
2017-05-08 18:03:16 -0400

    MERGE4

--- src/mytest/Test.java
+++ src/mytest/Test.java
@@ -3,7 +3,7 @@ package mytest;
 public class Test {
 
   public static void main(String[] args){
-    String aaa = "test man(Merge3)";
+    String aaa = "test man(Merge4)";
     System.out.println(aaa);
   }
 }
